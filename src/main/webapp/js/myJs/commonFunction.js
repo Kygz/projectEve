a7fa4be972891f2e6f69910fba6fd3bf758db8bf
量子风 <gavin110991@163.com>
2017-08-08 16:58:45 +0800

    登陆追加登陆人信息缓存

--- src/main/webapp/js/myJs/commonFunction.js
+++ src/main/webapp/js/myJs/commonFunction.js
@@ -23,3 +23,41 @@ var $message = {
         $("#alertDiv").modal('show');
     }
 };
+
+/**
+ * 存取设置数据 存于 sessionStorage
+ */
+var $designerStorage = {
+    /**
+     * 初始化 清空sessionStorage
+     */
+    storage : window.sessionStorage,
+    init : function () {
+        this.cleanAll();
+    },
+    getPropertyByKey : function(key){
+        var data = this.storage.getItem(key);
+        return data;
+    },
+    setPropertyByKey : function (key,value) {
+        var vivian = "";
+        if(value!==null && value!=="" && typeof value!=="undefined"){
+            vivian = value;
+        }
+        if(key){
+            this.storage.setItem(key,vivian);
+        }
+    },
+    delPropertyByEntityId : function (key) {
+        if(key){
+            this.storage.removeItem(key);
+        }
+    },
+    setCurrentMember :function (memberStr) {
+        this.setPropertyByKey("currentMember",memberStr);
+    },
+    getCurrentMember :function () {
+        var memberStr = this.getPropertyByKey("currentMember");
+        return $.parseJSON(memberStr,true);
+    }
+};
